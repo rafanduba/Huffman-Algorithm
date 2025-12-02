47bf6855a0498d00a2162cf35598c5130a8e878e
Rafael Rocha <rafaelrocha241003@gmail.com>
2025-12-02 07:26:05 -0300

    Print sentences and words frequences in output.dat file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include "InputReader.hpp"
 #include "TextProcessor.hpp"
 
@@ -8,6 +9,13 @@ int main() {
     InputReader reader;
 
     std::vector<std::string> textos = reader.lerTextos("../data/input.dat");
+
+    std::ofstream output_file("../data/output.dat");
+
+    if(!output_file.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo de saída.\n";
+        return 1;
+    }
 
     std::cout << "Total de blocos lidos: " << textos.size() << "\n\n";
 
@@ -22,5 +30,20 @@ int main() {
         }
         std::cout << "\n";
     }
+
+    for(size_t i = 0; i < textos.size(); i++) {
+        output_file << "=== Bloco " << i+1 << " ===\n";
+        output_file << textos[i] << "\n\n";
+        auto frequencia = TextProcessor::contarPalavras(textos[i]);
+
+        output_file << "Frequência de palavras:\n";
+        for(const auto& par : frequencia) {
+            output_file << par.first << ": " << par.second << "\n";
+        }
+        output_file << "\n";
+    }
+
+    output_file.close();
+    std::cout << "Resultados escritos em ../data/output.dat\n";
     return 0;
 }
